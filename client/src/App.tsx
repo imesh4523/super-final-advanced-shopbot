e957f6c93d47ed39e319c836f70b8fac351046c5
Your Name <you@example.com>
2026-04-20 11:52:36 +0000

    Set default theme to light

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -123,7 +123,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="dark" storageKey="shopeefy-theme">
+      <ThemeProvider defaultTheme="light" storageKey="shopeefy-theme">
         <TooltipProvider>
           <Toaster />
           <AdminNotifier />
